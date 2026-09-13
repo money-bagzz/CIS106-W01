4ae1772f3891e8790fb1c4ee6251821b2349990c
money-bagzz <ri74220@mail.harpercollege.edu>
2026-09-13 09:30:39 -0500

    Delete M3P5.py

--- M3P5.py
+++
@@ -1,6 +0,0 @@
-print("Enter the radius of a circle:")
-radius = float(input())
-area = 3.14 * (radius * radius)
-perimeter = (2 * 3.14) * radius
-
-print("Area: " + str(area) + "\nPerimeter: " + str(perimeter))
